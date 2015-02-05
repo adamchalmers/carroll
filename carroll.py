b452ade77b51e35b2a921198ebe983a9e1206b48
Adam <adam.s.chalmers@gmail.com>
2015-02-06 01:28:57 +1100

    Command line help now has descriptions.

--- carroll.py
+++ carroll.py
@@ -4,17 +4,21 @@ import click
 
 @click.group()
 def cli():
+    """Carroll is a command line tool for analysing propositional logic (also known as boolean functions or expressions).
+    """
     pass
 
 @cli.command()
 @click.argument("expression_1")
 @click.argument("expression_2")
 def equiv(expression_1, expression_2):
+    """Checks whether two expressions are logically equivalent."""
     print(truthtable.equivalent(expression_1, expression_2))
 
 @cli.command()
 @click.argument("expression")
 def table(expression):
+    """Outputs a truth table for a logical expression."""
     truthtable.print_truth_table(expression)
 
 if __name__ == "__main__":
