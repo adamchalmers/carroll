6ecaa12539c1bd197f479077f7e42958fe923f0f
Adam <adam.s.chalmers@gmail.com>
2015-02-06 00:54:12 +1100

    Exposed equivalence through command line.

--- carroll.py
+++ carroll.py
@@ -2,10 +2,20 @@ import truthtable
 import sys
 import click
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.argument("expression_1")
+@click.argument("expression_2")
+def equiv(expression_1, expression_2):
+    print(truthtable.equivalent(expression_1, expression_2))
+
+@cli.command()
 @click.argument("expression")
 def table(expression):
     truthtable.print_truth_table(expression)
 
 if __name__ == "__main__":
-    table()
+    cli()
